7748d843415503c64b829d4b593dff1d16ca90f3
Developer <developer@Developers-Mac-mini.local>
2023-11-12 13:50:25 +0530

    predifind function modify

--- FunctionDemo.java
+++ FunctionDemo.java
@@ -1,6 +1,7 @@
 import java.util.function.Function;
+
 //pridicate always output boolean
-// function output is anything and output is anything
+// function input is anything and output is anything
 public class FunctionDemo {
     public static void main(String[] args) {
         Function<String, String> fn = s -> s.concat(" Singh");
@@ -8,12 +9,12 @@ public class FunctionDemo {
         System.out.println(fn.apply("Dhiraj"));
         System.out.println(fn.apply("Niraj"));
 
-        String[] fruites = {"apple", "orange", "mango"};
+        String[] fruites = { "apple", "orange", "mango" };
 
         Function<String, String> fn1 = s -> s.concat(" is good for health");
         for (String frute : fruites) {
-        System.out.println(fn1.apply(frute));
-            
+            System.out.println(fn1.apply(frute));
+
         }
 
     }
